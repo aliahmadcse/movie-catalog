c5f633d09e7ce54542c315f1f519a63e30961ec1
Ali Ahmad <aliahmadcse@gmail.com>
2024-05-26 19:55:58 +0500

    init

---
+++ movie-info-service/src/test/java/codes/aliahmad/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package codes.aliahmad.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
---
+++ ratings-data-service/src/test/java/codes/aliahmad/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package codes.aliahmad.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
